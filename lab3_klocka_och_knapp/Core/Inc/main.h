34a89d20f2bb7323e2f85e902b702477394af584
Sara <saraodqvist@outlook.com>
2024-09-09 15:56:43 +0200

    lab3

--- lab3_klocka_och_knapp/Core/Inc/main.h
+++ lab3_klocka_och_knapp/Core/Inc/main.h
@@ -59,6 +59,9 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define MY_BTN_Pin GPIO_PIN_0
+#define MY_BTN_GPIO_Port GPIOC
+#define MY_BTN_EXTI_IRQn EXTI0_IRQn
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
