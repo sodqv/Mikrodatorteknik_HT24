72b3f2396a1f00680fecc77346966b328795b0cf
Sara <saraodqvist@outlook.com>
2024-09-27 16:20:14 +0200

    lab5 krångel

--- lab5_ADC/Core/Src/main.c
+++ lab5_ADC/Core/Src/main.c
@@ -18,10 +18,12 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "lcd.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "lcd.h"
+#include <stdio.h>
+#include <string.h>
 
 /* USER CODE END Includes */
 
@@ -41,18 +43,17 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc1;
-
-I2C_HandleTypeDef hi2c1;
-
-TIM_HandleTypeDef htim2;
-
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
+#define ADC_BUF_SIZE 3
+uint16_t adc_buffer[ADC_BUF_SIZE];
+int adc_buf_ix = 0;
+
 TextLCDType myLCD;
 
+uint8_t callback_occured = 0;
 uint16_t max_volt = 4095;
 
 char x_volt[20];
@@ -64,9 +65,6 @@ char y_volt[20];
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_ADC1_Init(void);
-static void MX_TIM2_Init(void);
-static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -74,8 +72,7 @@ static void MX_I2C1_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-
-// function that performs analog to digital conversion (ADC)
+/*// polling function that performs analog to digital conversion (ADC)
 uint16_t read_one_adc_value(ADC_HandleTypeDef *hadc)
 {
 	HAL_ADC_Start(hadc);						// starts the conversion process.
@@ -84,6 +81,19 @@ uint16_t read_one_adc_value(ADC_HandleTypeDef *hadc)
 	HAL_ADC_Stop(hadc);							// stops the conversion process.
 
 	return (uint16_t) reading;					// returns the converted value.
+}*/
+
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+	if (hadc -> Instance == ADC1)
+	{
+		callback_occured = 1;
+
+		adc_buffer[adc_buf_ix] = HAL_ADC_GetValue(hadc);
+		adc_buf_ix = (adc_buf_ix + 1) % ADC_BUF_SIZE;
+
+	}
 }
 
 
@@ -93,8 +103,9 @@ float normalize_12bit(uint16_t x)
 	float normalized = ((float)x / max_volt);
 
 	sprintf(x_volt, "%.2f", normalized);		// formats the normalized value into the x_volt string
-}
 
+	return normalized;
+}
 
 
 // numbers from -1 to 1
@@ -103,7 +114,11 @@ float normalize_12bit_posneg(uint16_t x)
 	float normalized = 2 * ((float)x / max_volt) - 1;
 
 	sprintf(x_volt, "%.2f", normalized);
+
+	return normalized;
 }
+
+
 
 
 /* USER CODE END 0 */
@@ -138,9 +153,6 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_ADC1_Init();
-  MX_TIM2_Init();
-  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -150,26 +162,48 @@ int main(void)
 
   TextLCD_Init(&myLCD, &hi2c1, 0x4E);
 
+  HAL_ADC_Start_IT(&hadc1);		//startar ADC-omvandling
+
+
+	/*
+  	  Callback sker varje gång en ADC conversion är klar.
+  	  Då ska värdet från conversion lagras i adc_buf_ix.
+	  Main loopen ska uppdatera displayen när ett nytt värde finns i adc_buf_ix
+	*/
+
+
   while (1)
   {
-      // Clear the LCD before displaying the new value
-      TextLCD_Clear(&myLCD);
 
-	  uint16_t conv_x_val = read_one_adc_value(&hadc1);
-
-
-	  normalize_12bit(conv_x_val);
-	  TextLCD_PutStr(&myLCD, x_volt);
+	  if (callback_occured == 1)
+	  	  {
+	  		  	//TextLCD_Clear(&myLCD);
 
 
-	  TextLCD_SetDDRAMAdr(&myLCD, 0x40);
-	  normalize_12bit_posneg(conv_x_val);
-	  TextLCD_PutStr(&myLCD, x_volt);
+	  			/* printar ut icke normaliserat värde från adc_buffer[0]
+	  		    char s[20];
+	  		    sprintf(s, "X: %d", adc_buffer[0]);
+	  		  	TextLCD_Home(&myLCD);
+	  			TextLCD_PutStr(&myLCD, s);
+	  			 */
 
 
+	  			//send value to the normalization function
+	  			normalize_12bit_posneg(adc_buffer[0]);
+
+	  			//printar det normaliserade x-värdet
+	  		  	TextLCD_Home(&myLCD);
+	  			TextLCD_PutStr(&myLCD, x_volt);
 
 
-      HAL_Delay(1000);  // Delay for 1 second before updating again
+	  			HAL_Delay(100);
+
+	  			callback_occured = 0;
+	  	  }
+
+	  	  HAL_Delay(100);
+
+
 
 
     /* USER CODE END WHILE */
@@ -223,137 +257,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_ADC1_Init(void)
-{
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_0;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
-
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM2_Init(void)
-{
-
-  /* USER CODE BEGIN TIM2_Init 0 */
-
-  /* USER CODE END TIM2_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM2_Init 1 */
-
-  /* USER CODE END TIM2_Init 1 */
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 84-1;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 4294967295-1;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM2_Init 2 */
-
-  /* USER CODE END TIM2_Init 2 */
-
 }
 
 /**
