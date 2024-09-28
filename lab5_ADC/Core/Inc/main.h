79d74c76168578eea533749a4e4bffc1d0a99859
Sara <saraodqvist@outlook.com>
2024-09-28 17:27:03 +0200

    y-value and x-value working

--- lab5_ADC/Core/Inc/main.h
+++ lab5_ADC/Core/Inc/main.h
@@ -61,6 +61,8 @@ void Error_Handler(void);
 #define B1_GPIO_Port GPIOC
 #define xValue_Pin GPIO_PIN_0
 #define xValue_GPIO_Port GPIOA
+#define yValue_Pin GPIO_PIN_1
+#define yValue_GPIO_Port GPIOA
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
--- lab5_ADC/Core/Src/stm32f4xx_hal_msp.c
+++ lab5_ADC/Core/Src/stm32f4xx_hal_msp.c
@@ -100,11 +100,12 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**ADC1 GPIO Configuration
     PA0-WKUP     ------> ADC1_IN0
+    PA1     ------> ADC1_IN1
     */
-    GPIO_InitStruct.Pin = xValue_Pin;
+    GPIO_InitStruct.Pin = xValue_Pin|yValue_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(xValue_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* ADC1 interrupt Init */
     HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
@@ -135,8 +136,9 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
 
     /**ADC1 GPIO Configuration
     PA0-WKUP     ------> ADC1_IN0
+    PA1     ------> ADC1_IN1
     */
-    HAL_GPIO_DeInit(xValue_GPIO_Port, xValue_Pin);
+    HAL_GPIO_DeInit(GPIOA, xValue_Pin|yValue_Pin);
 
     /* ADC1 interrupt DeInit */
     HAL_NVIC_DisableIRQ(ADC_IRQn);
